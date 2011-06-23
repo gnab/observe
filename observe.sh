e8ab76a07925a2c2546757752242c31f0fde1d00
Ole Petter Bang <olepbang@gmail.com>
2011-06-23 19:00:43 +0200

    Fixed ignore bug caused by asterix expansion issue.

--- observe.sh
+++ observe.sh
@@ -10,6 +10,7 @@ observe()
   PATHS=""
   while [ $# -gt 0 ]; do
     case $1 in
+
       "-e")
         if [ $# -lt 2 ]; then
           echo "Missing command after -e parameter."
@@ -19,15 +20,17 @@ observe()
         CMD=$1
         echo "Executing \"$CMD\" when any files are modified"
       ;;
+
       "-i")
         if [ $# -lt 2 ]; then
           echo "Missing filter after -i parameter."
           return
         fi
         shift
-        IGNORE="-not -name $1"
-        echo "Ignoring files matching filter \"$1\""
+        IGNORE=$1
+        echo "Ignoring files matching filter \"$IGNORE\""
       ;;
+
       "-h")
         echo "Usage: observe [OPTION]... [PATH]..."
         echo
@@ -56,18 +59,28 @@ observe()
     done
   fi
 
+  FIND="find -L $PATHS -type f -newermt"
   NOW=`date`
+
   while true; do 
+
     while true; do
-      MODIFIED=`find -L $PATHS -type f -newermt "$NOW" $IGNORE`
+      if [ -n "$IGNORE" ]; then
+        MODIFIED=`$FIND "$NOW" -not -name "$IGNORE"`
+      else
+        MODIFIED=`$FIND "$NOW"`
+      fi      
+
       if [ -n "$MODIFIED" ]; then
         break
       fi
       sleep 1
     done
+
     if [ -n "$!" ]; then
       kill -0 $! && kill $! && sleep 1
     fi
+
     if [ -n "$CMD" ]; then
       # TODO: Make background process optional with parameter
       $CMD #&
@@ -76,6 +89,7 @@ observe()
         echo $file
       done
     fi
+
     sleep 1
     NOW=`date`
   done
